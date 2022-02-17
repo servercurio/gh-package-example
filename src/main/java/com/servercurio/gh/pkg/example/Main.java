25233953962665c64f9ee25a942256cf2b67f1c6
Nathan Klick <nathan.klick@servercurio.com>
2022-02-17 02:10:58 -0600

    Initial POC
    Signed-off-by: Nathan Klick <nathan.klick@servercurio.com>

---
+++ src/main/java/com/servercurio/gh/pkg/example/Main.java
@@ -0,0 +1,14 @@
+package com.servercurio.gh.pkg.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+
+	private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+
+	public static void main(final String[] args) {
+		LOG.info("Hello World");
+	}
+
+}
